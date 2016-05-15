24a7d8603d18f64de29d67e2051a2854b25dd2b6
fathergoose <al@ilseman.com>
2016-05-15 14:27:24 -0500

    removed graduates

--- app/models/graduate.rb
+++
@@ -1,4 +0,0 @@
-class Graduate < ActiveRecord::Base
-  validates :first_name, :last_name, :email, :graduation_date, presence: true
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
-end
