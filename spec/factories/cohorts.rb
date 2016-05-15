7ba2224dd9128288d3c5b2e6cdaed691f62abe5a
dianacamacho <dcam1901@yahoo.com>
2016-05-15 12:35:56 -0500

    initiated cohorts model and controller

---
+++ spec/factories/cohorts.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :cohort do
+    name "MyString"
+    campus "MyString"
+    instructor "MyString"
+    cohort_date "MyString"
+  end
+end
