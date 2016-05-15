bc35566c2b5e6d8b84a7cd3d69da385f7c339f2f
fathergoose <al@ilseman.com>
2016-05-15 13:17:35 -0500

    validates cohort_id only for non-admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
 
   belongs_to :cohort
 
-  validates :first_name, :last_name, :email, :graduation_date, :cohort_id, presence: true
+  validates :first_name, :last_name, :email, :graduation_date, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :cohort_id, presence: true unless user.admin
 
   def full_name
     "#{first_name.capitalize} #{last_name.capitalize}"
