e043498f04e2ee2ed9aa879dd8380b87e8944e2c
fathergoose <al@ilseman.com>
2016-05-15 13:04:06 -0500

    changed to a user' '

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe Graduate do
+  describe User do
 
     it "has a valid factory" do
       expect(FactoryGirl.build(:user)).to be_valid
