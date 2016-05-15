6c46333bcd40231a5be856de6bfe59d5b46ad150
fathergoose <al@ilseman.com>
2016-05-15 15:00:49 -0500

    updated schema: '

--- db/schema.rb
+++ db/schema.rb
@@ -34,28 +34,15 @@ ActiveRecord::Schema.define(version: 20160515193444) do
     t.date     "graduation_date"
     t.integer  "cohort_id"
     t.integer  "user_type"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "phone"
-    t.boolean  "admin",            default: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "current_title"
-    t.string   "current_employer"
-    t.date     "graduation_date"
-    t.integer  "cohort_id"
-    t.integer  "user_type"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.boolean  "admin",                  default: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
