b8b9439e80157b312615f4c9fd2c627ff1f9acef
dianacamacho <dcam1901@yahoo.com>
2016-05-15 13:04:54 -0500

    removed user_id from users validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
   belongs_to :cohort
 
-  validates :first_name, :last_name, :email, :graduation_date, :cohort_id, :user_id, presence: true
+  validates :first_name, :last_name, :email, :graduation_date, :cohort_id, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 
   def full_name
