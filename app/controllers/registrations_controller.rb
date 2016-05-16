9ad260097ff0e2d0942c561b04b685b3388a73d6
fathergoose <al@ilseman.com>
2016-05-15 19:31:24 -0500

    added #account_update_params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,28 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :phone, :email, :current_title, :password, :password_confirmation, :graduation_date, :cohort_id)
+    params.require(:user).permit(:first_name,
+                                 :last_name, 
+                                 :phone, 
+                                 :email, 
+                                 :current_title, 
+                                 :graduation_date, 
+                                 :cohort_id, 
+                                 :password, 
+                                 :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name,
+                                 :last_name, 
+                                 :phone, 
+                                 :email, 
+                                 :current_title, 
+                                 :graduation_date, 
+                                 :cohort_id, 
+                                 :password, 
+                                 :password_confirmation,
+                                 :current_password)
   end
 
 end
