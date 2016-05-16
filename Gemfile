62d4cc660a018f0e2196246ef9028af84653d33c
dianacamacho <dcam1901@yahoo.com>
2016-05-15 23:41:45 -0500

    angular setup

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'devise'
+gem 'rails-assets-angular', source: 'https://rails-assets.org'
 # Use Unicorn as the app server
 # gem 'unicorn'
 
---
+++ app/assets/javascripts/users_ctrl.js
@@ -0,0 +1,15 @@
+(function() {
+  "use strict";
+
+  angular.module("app").controller("usersCtrl", ['$scope', '$http', function($scope, $http){
+
+    $scope.setupUsersIndex = function(){
+      $http.get('/api/v1/users.json').then(function(response){
+        $scope.users = response.data;
+      });
+    };
+
+    window.scope = $scope;
+  }]);
+
+}());
